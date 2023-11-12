265e021249509cb26a4acc7c5bd93609573c4786
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-11-12 09:13:53 +0100

    Updated user

--- controllers/auth.js
+++ controllers/auth.js
@@ -105,7 +105,7 @@ const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.local.password)
         if (!isMatch) return res.status(403).json({ message: "Invalid Password" })
         const token = jwt.sign({ email: user.local.email }, process.env.JWT_SECRET, { expiresIn: '1h' })
-        res.status(200).json({ message: "Login successful", token: token, email: user.local.email, firstName: user.firstName, lastName: user.lastName })
+        res.status(200).json({ message: "Login successful", token: token, email: user.local.email, firstName: user.firstName, lastName: user.lastName, avatarUrl: user.avatar.avatarUrl, avatarName: user.avatar.avatarName })
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: error.message })
@@ -114,7 +114,7 @@ const login = async (req, res) => {
 
 const updateProfile = async (req, res) => {
     try {
-        const { firstName, lastName, gender, role, bio, companyIndustry, country, topics, headshot, socials } = req.body
+        const { firstName, lastName, gender, role, bio, companyIndustry, country, topics, headshot, availableFor, socials } = req.body
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             const errorMessages = errors.array().map(error => error.msg)
@@ -127,6 +127,7 @@ const updateProfile = async (req, res) => {
         user.headshot = headshot
         user.role = role
         user.bio = bio
+        user.availableFor = availableFor
         user.topics = topics
         user.socials = socials
         user.companyIndustry = companyIndustry
